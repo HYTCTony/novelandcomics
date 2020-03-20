557e833869745cfb28ced746f2915032a3d2d256
coder_bill <coder_bill@163.com>
2020-03-20 22:15:06 +0800

    Bill---2020.3.20次改会员时间戳转时间（毫秒 1000）

--- app/src/main/java/com/huli/foxread/ui/activities/MyPrivilegeActivity.java
+++ app/src/main/java/com/huli/foxread/ui/activities/MyPrivilegeActivity.java
@@ -379,7 +379,7 @@ public class MyPrivilegeActivity extends BaseActivity implements View.OnClickLis
                             UserInfoCache.saveCacheAll(MyPrivilegeActivity.this, data);
                             tvVipTypeTitle.setText(R.string.txt_monthly_vip);
                             tvVipTime.setText(String.format(getString(R.string.txt_vip_end_time_colon),
-                                    DateTimeUtil.formatDateTime(UserInfoCache.getVipEndtime(MyPrivilegeActivity.this), "yyyy-MM-dd")));
+                                    DateTimeUtil.formatDateTime(UserInfoCache.getVipEndtime(MyPrivilegeActivity.this) * 1000, "yyyy-MM-dd")));
                             tvVipTips.setText(R.string.txt_tips_vip_state);
                             tvAccountSetup.setVisibility(View.GONE);
                             ivIconVipSymbol.setVisibility(View.VISIBLE);
